cd4e38b92e7c024afedd8d285680a660c1ebf667
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2024-12-29 20:56:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-	console.log(c)
+let map=new Map();
+	for(let t of str){
+		if(map.has(t)){
+			map.set(t,map.get(t)+1)
+		}
+		else{
+			map.set(t,1)
+		}
+	}
+	for(let char of map){
+		if(char[1]==1){
+			return char[0]
+		}
+		else{
+			return null;
+		}
+	}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
